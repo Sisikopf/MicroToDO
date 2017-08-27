fc652c0d289db9601b38395097a8d99439ba9a1e
sisikopf <dimko12@gmail.com>
2017-08-27 17:36:31 +0300

    Added dev server to project

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
+import main from './scss/main.scss'
 
-[1,2,3].forEach(num => console.log("hello!: " + num));
+[1,2,3,4,5,6].forEach(num => console.log("hello!: " + num));
+console.log(main);
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,8 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    devtool: 'inline-source-map',
+
     module: {
         rules: [
             { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
