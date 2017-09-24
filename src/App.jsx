e537b7da1e9030bb7597b99d25fa9e42223fbe95
sisikopf <dimko12@gmail.com>
2017-09-24 22:31:23 +0300

    Added react to dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import Main from './components/Main';
 // eslint-disable-next-line no-unused-vars
 import main from './scss/main.scss';
 
+const store = createStore();
 
 ReactDom.render(
-  <BrowserRouter>
-    <Main />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Main />
+    </BrowserRouter>
+  </Provider>,
   // eslint-disable-next-line no-undef
   document.getElementById('app'));
